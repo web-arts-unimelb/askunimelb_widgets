6074497a5423c2596f93a709942ddc09d00be450
Peter Lieverdink <peter.lieverdink@unimelb.edu.au>
2013-03-01 10:26:40 +1100

    Pass sanitised widget settings through Drupal.behaviors and process them via external JS on page load.

---
+++ askunimelb_widgets.js
@@ -0,0 +1,24 @@
+/**
+ * @file: askunimelb_widgets.js
+ *
+ * Process module settings and add FAQ widgets to the page if required.
+ */
+(function ($) {
+  Drupal.behaviors.askunimelb_widgets = {
+    attach: function(context) {
+      var widgets = Drupal.settings.askunimelb_widgets;
+
+      // Loop over the widget settings array.
+      for (var i = 0; i < widgets.length; i++) {
+
+        // Add a widget into each detected widget div.
+        if ($('#askunimelb-widgets-widget-' + widgets[i].delta).length != 0) {
+          RightNow.Client.Controller.addComponent(
+            widgets[i],
+            '//studentadmin-unimelb.widget.custhelp.com/ci/ws/get'
+          );
+        }
+      }
+    }
+  }
+})(jQuery);
